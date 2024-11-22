babccd89e8580d80530476b12233b7a8e3c0feef
Doaa Awan <106412925+Doaa-Awan@users.noreply.github.com>
2024-11-21 22:56:59 -0500

    Initialize project
    - deleted unnecessary files
    - added icon for document
    - installed vite and bootstrap
    - created Hero section

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Hero from "./sections/Hero/hero";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <>
+      <Hero />
+    </>
+  );
+}
+
+export default App;
---
+++ src/sections/Hero/hero.jsx
@@ -0,0 +1,7 @@
+import styles from "./HeroStyles.module.css";
+
+function Hero() {
+  return <div className="btn btn-primary m-5">testing</div>;
+}
+
+export default Hero;
