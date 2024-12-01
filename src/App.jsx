bf1d24ad20e40ddc1955af5fa356559f271f07e8
Doaa Awan <106412925+Doaa-Awan@users.noreply.github.com>
2024-11-30 23:56:19 -0500

    Add pages and navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import "./App.css";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Experience from "./pages/Experience";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import Nav from "./sections/Nav/Nav";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Nav />
+      <div className="page-container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Experience" element={<Experience />} />
+          <Route path="/Projects" element={<Projects />} />
+        </Routes>
+      </div>
     </>
   );
 }
--- src/sections/Nav/Nav.jsx
+++ src/sections/Nav/Nav.jsx
@@ -1,11 +1,13 @@
+import { Link, NavLink } from "react-router-dom";
+
 function Nav() {
   return (
     <nav className="navbar navbar-expand-lg bg-transparent fixed-top p-3 ms-3 me-3 mt-2">
       <div className="container-fluid">
         {/* Logo */}
-        <a className="navbar-brand" href="#">
-          Doaa Awan
-        </a>
+        <Link className="navbar-brand" to="/">
+          D.A
+        </Link>
         {/* Toggle Button */}
         <button
           className="navbar-toggler shadow-none border-0"
@@ -20,7 +22,7 @@ function Nav() {
         {/* Sidebar */}
         <div
           className="sidebar offcanvas offcanvas-end"
-          tabindex="-1"
+          tabIndex="-1"
           id="offcanvasNavbar"
           aria-labelledby="offcanvasNavbarLabel"
         >
@@ -39,34 +41,34 @@ function Nav() {
           <div className="offcanvas-body d-flex flex-column flex-lg-row p-4 p-lg-0">
             <ul className="navbar-nav justify-content-center align-items-center fs-5 flex-grow-1 pe-3">
               <li className="nav-item mx-2">
-                <a className="nav-link active" aria-current="page" href="#">
+                <NavLink className="nav-NavLink" aria-current="page" to="/">
                   Home
-                </a>
+                </NavLink>
               </li>
               <li className="nav-item mx-2">
-                <a className="nav-link" href="#">
-                  About
-                </a>
-              </li>
-              <li className="nav-item mx-2">
-                <a className="nav-link" href="#">
+                <NavLink className="nav-NavLink" to="/Projects">
                   Projects
-                </a>
+                </NavLink>
               </li>
               <li className="nav-item mx-2">
-                <a className="nav-link" href="#">
+                <NavLink className="nav-NavLink" to="/Experience">
                   Experience
-                </a>
+                </NavLink>
+              </li>
+              <li className="nav-item mx-2">
+                <NavLink className="nav-NavLink" to="/About">
+                  About
+                </NavLink>
               </li>
             </ul>
             {/* Contact Button */}
             <div className="d-flex flex-lg-row justify-content-center align-items-center gap-3">
-              <a
-                href="#"
+              <Link
+                to="/"
                 className="btn-contact text-white text-decoration-none px-4 py-2 rounded-4"
               >
                 Contact
-              </a>
+              </Link>
             </div>
           </div>
         </div>
